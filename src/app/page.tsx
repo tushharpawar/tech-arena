1d6c5b05c6c59b9369d3994270ebd32ad265fe0c
Tushar Pawar <tushharpawar14@gmail.com>
2025-02-03 23:49:00 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import cuid from "cuid";
+import { useRouter } from "next/navigation";
+
+
 
 export default function Home() {
+  
+  const callId = cuid()
+  const router = useRouter()
+  
+  const createMeeting = () =>{
+        router.push(`/dashboard/${callId}`)
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        Hello
+        <Button onClick={createMeeting}>Create Meeting</Button>
+        <Button variant='secondary'>Join Meeting</Button>
     </div>
   );
 }
