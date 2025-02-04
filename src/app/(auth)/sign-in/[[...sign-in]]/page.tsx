0a9310d30e691efd23dee2aeb1b18354b6917f17
Tushar Pawar <tushharpawar14@gmail.com>
2025-02-04 23:58:34 +0530

    Update page.tsx

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Page (){
-    return <SignIn/>
+    return (
+        <>
+        <div className="h-screen w-screen flex items-center justify-center">
+        <SignIn appearance={{baseTheme:dark}}/>
+        </div>
+        </>
+    )
+    
 }
